097de8c205f5bf0f380a3cb844896891a51b100c
Fumiaki Yoshimatsu <fumiaki@madolabs.co>
2019-01-22 17:35:16 -0500

    Color picker.

--- lib/glow_view_state.dart
+++ lib/glow_view_state.dart
@@ -10,19 +10,62 @@ class GlowViewState extends State<GlowView> {
 
   GlowViewState(this._dimLevel, this._color);
 
+  _onRadioChanged(value) {
+    setState(() {
+      _color = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: _color,
-      child: Slider(value: _dimLevel, onChanged: (value) {
-        print(">>> ${value}");
-        methodChannel.invokeMethod("setDimLevel", value).then((dimValue) {
-          setState(() {
-            _color = Colors.red;
-            _dimLevel = dimValue;
-          });
-        });
-      }),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Slider(value: _dimLevel, onChanged: (value) {
+            methodChannel.invokeMethod("setDimLevel", value).then((dimValue) {
+              setState(() {
+                _dimLevel = dimValue;
+              });
+            });
+          }),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Radio(
+                value: Colors.yellow,
+                groupValue: _color,
+                onChanged: _onRadioChanged,
+              ),
+              Text(
+                "Yellow",
+                style: TextStyle(fontSize: 16.0),
+              ),
+              Radio(
+                value: Colors.lightGreen,
+                groupValue: _color,
+                onChanged: _onRadioChanged,
+              ),
+              Text(
+                "Green",
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+              Radio(
+                value: Colors.purple,
+                groupValue: _color,
+                onChanged: _onRadioChanged,
+              ),
+              Text(
+                "Purple",
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ],
+          ),
+        ],
+      )
     );
   }
 }
